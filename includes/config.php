de1ffb6f7d6a4ec08809792ed4f6cd0483b117ed
JACM701 <josuechucmedina980@gmail.com>
2025-06-03 13:40:17 -0600

    cambio

--- includes/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-if (!defined('DB_HOST')) define('DB_HOST', 'localhost');     // Set database host
-if (!defined('DB_USER')) define('DB_USER', 'root');         // Set database user
-if (!defined('DB_PASS')) define('DB_PASS', '');             // Set database password
-if (!defined('DB_NAME')) define('DB_NAME', 'naycs');      // Set database name
-
-?>
